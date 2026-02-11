49f3a0c9514497ecb8f8b059d28d26ef39242a10
rpenya <rpenya.mail@gmail.com>
2026-02-11 17:44:23 +0100

    add files

---
+++ Example/CoreDataClientExampleApp/CoreDataClientExampleApp/ExampleView/ExampleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExampleViewModel.swift
+//  CoreDataClientExampleApp
+//
+//  Created by Raul Peña on 11/2/26.
+//
+import Foundation
+
+@MainActor
+final class ExampleViewModel: ObservableObject {
+    
+}
---
+++ Sources/CoreDataClient/CoreDataError.swift
@@ -0,0 +1,7 @@
+//
+//  CoreDataClientError.swift
+//  CoreDataClient
+//
+//  Created by Raul Peña on 11/2/26.
+//
+
