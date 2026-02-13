4daba241028168bf379d412429e9cd918bc975c5
rpenya <rpenya.mail@gmail.com>
2026-02-13 16:08:22 +0100

    remove tests placeholder

--- Tests/CoreDataClientTests/ios_coredata_clientTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import ios_coredata_client
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
