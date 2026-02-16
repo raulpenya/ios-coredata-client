ece3a81e0db669e054f084933faf8d9b969e3b65
rpenya <rpenya.mail@gmail.com>
2026-02-16 16:09:06 +0100

    create test model

--- Tests/CoreDataClientTests/Mocks/MockPersistentStore.swift
+++ Tests/CoreDataClientTests/Mocks/MockPersistentStore.swift
@@ -9,16 +9,58 @@ import CoreDataClient
 import CoreData
 
 final class MockPersistentStore: PersistentStoreProtocol {
-    var container: NSPersistentContainer = {
+    let container: NSPersistentContainer
+    
+    init() {
+        let model = Self.makeModel()
+        container = NSPersistentContainer(
+            name: "TestModel",
+            managedObjectModel: model
+        )
         let description = NSPersistentStoreDescription()
-        description.url = URL(fileURLWithPath: "/dev/null")
-        let container = NSPersistentContainer(name: "your_model_name")
+        description.type = NSInMemoryStoreType
         container.persistentStoreDescriptions = [description]
         container.loadPersistentStores { _, error in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+            if let error {
+                fatalError("Unresolved error \(error)")
             }
         }
-        return container
-    }()
+    }
+    
+    private static func makeModel() -> NSManagedObjectModel {
+        let model = NSManagedObjectModel()
+        
+        let entity = NSEntityDescription()
+        entity.name = "PersonLocalEntity"
+        entity.managedObjectClassName = NSStringFromClass(PersonLocalEntity.self)
+        
+        let name = NSAttributeDescription()
+        name.name = "name"
+        name.attributeType = .stringAttributeType
+        name.isOptional = true
+        
+        let email = NSAttributeDescription()
+        email.name = "email"
+        email.attributeType = .stringAttributeType
+        email.isOptional = true
+        
+        entity.properties = [name, email]
+        model.entities = [entity]
+        
+        return model
+    }
+    
+    func insertPerson(person: Person) {
+        let context = container.viewContext
+        _ = try! Person.persons.first!.insert(into: context)
+        try! context.save()
+    }
+    
+    func insertPersonWithNilName(person: Person) {
+        let context = container.viewContext
+        let entity = PersonLocalEntity(context: context)
+        entity.name = nil
+        entity.email = person.email
+        try! context.save()
+    }
 }
