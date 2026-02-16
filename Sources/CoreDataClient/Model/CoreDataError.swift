c78e1250d6bcd13def72a570d1d73e290c32b23f
rpenya <rpenya.mail@gmail.com>
2026-02-16 16:49:58 +0100

    coredataerror equatable

--- Sources/CoreDataClient/Model/CoreDataError.swift
+++ Sources/CoreDataClient/Model/CoreDataError.swift
@@ -5,10 +5,24 @@
 //  Created by Raul Peña on 11/2/26.
 //
 
-public enum CoreDataError: Error {
+public enum CoreDataError: Error, Equatable {
     case fetchFailed(Error)
     case insertFailed(Error)
     case deleteFailed(Error)
     case batchDeleteFailed(Error)
     case unknown(Error)
+    
+    public static func == (lhs: CoreDataError, rhs: CoreDataError) -> Bool {
+        switch (lhs, rhs) {
+        case (.fetchFailed, .fetchFailed),
+            (.insertFailed, .insertFailed),
+            (.deleteFailed, .deleteFailed),
+            (.batchDeleteFailed, .batchDeleteFailed),
+            (.unknown, .unknown):
+            return true
+            
+        default:
+            return false
+        }
+    }
 }
